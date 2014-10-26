4750e65a9ee131a2de3271b029f0520d28516000
unknown <3203317@qq.com>
2014-10-26 16:50:36 +0800

    haha

---
+++ server/lib/util/appUtil.js
@@ -0,0 +1,19 @@
+/*!
+ * h9n-server
+ * Copyright(c) 2014 huangxin <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+var path = require('path'),
+	fs = require('fs'),
+	async = require('async');
+
+var utils = require('../../../shared/utils'),
+	Constants = require('./constants');
+
+var exp = module.exports;
+
+exp.defaultConfiguration = function(app){
+	// TODO
+};
