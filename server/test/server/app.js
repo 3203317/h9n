10fcdfd76a85fab300fb848fd8e8bc8d968cdcf5
unknown <3203317@qq.com>
2014-10-26 20:33:44 +0800

    haha

--- server/test/server/app.js
+++ server/test/server/app.js
@@ -8,22 +8,27 @@
 var utils = require('../../../shared/utils'),
 	speedt = require('../../');
 
+process.on('uncaughtException', function (err){
+	if(err){
+		console.error('[%s] Caught exception: %j.', utils.format(), err.stack);
+	}
+});
+
+process.on('exit', function (code){
+	if(0 === code){
+		console.log('[%s] Process exit.', utils.format())
+		return
+	}
+	console.error('[%s] Process exit with code: %s.', utils.format(), code)
+});
+
 var app = speedt.createApp(null, function (err){
 	var self = this;
 	self.set('name', 'uplserv');
 });
 
-
 app.start(function (err){
 	if(err){
 		console.error('[%s] App start error: %s.', utils.format(), err.message);
 	}
-});
-
-process.on('uncaughtException', function (err){
-	// TODO
-});
-
-process.on('exit', function (code){
-	// TODO
 });
