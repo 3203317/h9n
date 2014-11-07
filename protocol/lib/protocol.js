3d58c69abf156e82f37018dd40df6093cccec5fd
unknown <3203317@qq.com>
2014-11-07 21:56:40 +0800

    haha

---
+++ protocol/lib/protocol.js
@@ -0,0 +1,12 @@
+/*!
+ * h9n-protocol
+ * Copyright(c) 2014 huangxin <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+(function (exports, ByteArray, global){
+	var Protocol = exports;
+	console.log(arguments)
+
+})('undefined' === typeof(window) ? module.exports : (this.Protocol = {}));
---
+++ protocol/test/protocol.js
@@ -0,0 +1,19 @@
+/*!
+ * h9n-protocol
+ * Copyright(c) 2014 huangxin <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+var should = require('should');
+var Protocol = require('../');
+var Package = Protocol.Package;
+var Message = Protocol.Message;
+
+describe('H9n protocol test', function(){
+	describe('String encode and decode', function(){
+    	it('should be ok to encode and decode Chinese string', function(){
+    		// TODO
+    	});
+	});
+});
